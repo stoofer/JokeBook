ab8388933b6aa16d46c19113f541472cc5aa5aef
Stuart Caborn <stooferus@gmail.com>
2011-05-03 22:40:59 +0100

    Add a (bad) joke

--- joke-book/src/joke_book/core.clj
+++ joke-book/src/joke_book/core.clj
@@ -1,11 +1,14 @@
 (ns joke-book.core
+  (:use compojure.core)
   (:require [appengine-magic.core :as ae]))
 
 
 (defn joke-book-app-handler [request]
   {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello, world!"})
+   :headers {"Content-Type" "text/html"}
+   :body (str
+	  "<html><body><p>What's small brown and sticky?</p>"
+	  "<p>A stick!</p></body></html>")})
 
 
 (ae/def-appengine-app joke-book-app #'joke-book-app-handler)

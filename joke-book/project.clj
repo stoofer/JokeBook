e8fb94714fcc2bed1848636e9a6bb12760f317a1
Stuart Caborn <stooferus@gmail.com>
2011-06-13 16:05:10 +0100

    Use atom to store canned jokes in memory and simulate required behavior of backing store

--- joke-book/project.clj
+++ joke-book/project.clj
@@ -2,6 +2,7 @@
   :description "Fun project to learn clojue and GAE"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [compojure "0.6.2"]
+                 [clj-time "0.1.0-RC1"]
 				         [hiccup "0.3.5"]]
   :dev-dependencies [[appengine-magic "0.4.1"]
                      [lein-eclipse "1.0.0"]])
--- joke-book/src/joke_book/jokes.clj
+++ joke-book/src/joke_book/jokes.clj
@@ -1,12 +1,30 @@
 (ns joke-book.jokes
-  (:require [clojure.string :only [split-lines] :as string]))
+  (:require [clojure.string :only [split-lines] :as string])
+  (:use [clj-time.core :only [now]]))
+
+(def jokes (atom []))
 
 (defn random []
-  {:lines '("What's small brown and sticky?"
-            "A stick!")})
+  (rand-nth @jokes))
 
 (defn parse [{:keys [lines]}]
-  {:lines (string/split-lines lines)})
+  {
+   :lines (string/split-lines lines)
+   :status :unmoderated
+   :created (now)
+  })
 
 (defn save [joke]
-  (defn random [] joke))
+  (swap! jokes conj joke))
+
+;TODO - move this out
+;Test data only
+(def canned-jokes '("What's small brown and sticky?\nA stick!" 
+                    "I say, I say, I say\nMy dog has no nose.\nHow does he smell?\nAwful!"
+                   "What's black, white and red all over?\nA sunburnt penguin."))
+(defn line-to-joke [line]
+  (-> {:lines line}
+      parse
+      save))
+
+(doall (map line-to-joke canned-jokes))
